549387235dd7c0be5f22618dd2ee85c4b4052080
Salem Kallien <31890883+SalemKitkat@users.noreply.github.com>
2018-04-23 10:47:52 +0100

    remove v4 prep, use https://github.com/ShadowRain-Revived/ShadowRain/tree/SalemKitkat-v4prep instead

--- CommandEngine.bat
+++ CommandEngine.bat
@@ -4,7 +4,6 @@ If "%1"=="" Goto NO_PARAM
 If "%1"=="-v" If "%2"=="0.0.1" Goto 0.0.1
 If "%1"=="-v" If "%2"=="0.0.2" Goto 0.0.2
 If "%1"=="-v" If "%2"=="0.0.3" Goto 0.0.3
-rem ; If "%1"=="-v" If "%2"=="0.0.4" Goto 0.0.4
 Exit
 
 :NO_PARAM
@@ -20,6 +19,3 @@ modules\0.0.2.bat -z2
 
 :0.0.3
 modules\0.0.3.bat -z3
-
-:0.0.4
-modules\0.0.4.bat -z4
